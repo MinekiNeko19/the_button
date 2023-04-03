628eceb8cda2e1d5e820e5b8b4e19fc175bfc170
Sarah Leong <sarahleong19@gmail.com>
2023-04-02 20:25:21 -0400

    counts how many times the button is pressed; though its ugly rn

--- thebutton.js
+++ thebutton.js
@@ -1,7 +1,26 @@
+var timesPressed = 0;
+
+function counter() {
+  timesPressed++;
+}
+
+function buttonTask() {
+  timesPressed++;
+  // console.log(timesPressed);
+  document.getElementById("count").innerHTML = timesPressed;
+
+  var o = Math.floor(Math.random() * 2); // * number of fxns +1 made yes
+
+  if (o == 1) {
+    changeBGColor();
+  }
+}
+
+
 function changeBGColor() {
-  var r = Math.floor(Math.random()*255);  
-  var g = Math.floor(Math.random()*255);
-  var b = Math.floor(Math.random()*255);
+  var r = Math.floor(Math.random()*256);  
+  var g = Math.floor(Math.random()*256);
+  var b = Math.floor(Math.random()*256);
 
   let color = "#" + r.toString(16) + g.toString(16) + b.toString(16);
 
