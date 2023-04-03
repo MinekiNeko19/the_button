4d0651749a6300a5f1e06ddc9dd321e2abac08f5
Sarah Leong <sarahleong19@gmail.com>
2023-04-02 20:02:35 -0400

    fixed the format of the colors; ty hebe

--- thebutton.js
+++ thebutton.js
@@ -3,7 +3,7 @@ function changeBGColor() {
   var g = Math.floor(Math.random()*255);
   var b = Math.floor(Math.random()*255);
 
-  let color = "#" + r.toString() + g.toString() + b.toString();
+  let color = "#" + r.toString(16) + g.toString(16) + b.toString(16);
 
   document.body.style.backgroundColor = color;
 }
